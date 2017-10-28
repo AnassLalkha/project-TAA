265ca8a9e1e49e768bf544ac63f74e971cd89ec8
Hakima <hkebaili92@gmail.com>
2017-10-28 12:32:26 +0200

    add the consumer constant and oepration enum

--- src/main/java/istic/taa/project/constants/Constants.java
+++ src/main/java/istic/taa/project/constants/Constants.java
@@ -4,8 +4,18 @@ import istic.taa.project.utils.ConstantsBuilderUtils;
 
 public class Constants {
 	public static final String SALT = ConstantsBuilderUtils.getInstance().getPropsByKey("application.salt");
+	public static final String WEATHER_API_KEY = ConstantsBuilderUtils.getInstance().getPropsByKey("weather.api.key");
+	public static final String WEATHER_ENDPOINT = ConstantsBuilderUtils.getInstance()
+			.getPropsByKey("weather.api.endpoint");
+	public static final String WEATHER_ENDPOINT_DAYS = ConstantsBuilderUtils.getInstance().getPropsByKey("weather.endpoint.days");
+	public static final String WEATHER_MODE_FORECAST = ConstantsBuilderUtils.getInstance().getPropsByKey("weather.mode.forecast");
+	public static final String WEATHER_MODE_CURRENT = ConstantsBuilderUtils.getInstance().getPropsByKey("weather.mode.current");
+	public static final String LOCATION_PARAMETER = "${APPLOC}";
+	public static final String MODE_PARAMETER = "${MODE}";
+	public static final String APPID_PARAMETER = "${APPKEY}";
+	public static final String DAYS_PARAMETER = "${days}";
 
-	// private constructor to make it not instanciable 
+	// private constructor to make it not instanciable
 	private Constants() {
 
 	}
---
+++ src/main/java/istic/taa/project/constants/Operations.java
@@ -0,0 +1,23 @@
+package istic.taa.project.constants;
+
+public enum Operations {
+	UPDATE_WEATHER("UPDATING WEATHER"),
+	REQUEST_DELETION("REQUEST DELETION CODE"),
+	CREATE_USER("CREATE USER"),
+	AUTHENTICATION("AUTHENTICATION"),
+	CHECK_EMAIL("CHECK PARAMETER EMAIL"),
+	CHECK_USERNAME("CHECK PARAMETER USERNAME"),
+	FORECAST_WEATHER("GET FORCAST WEATHER"),
+	CURRENT_WEATHER("GET CURRENT WEATHER");
+	
+	private String name;
+	private Operations(String str) {
+		 name = str;
+	}
+	@Override
+	public String toString() {
+		return name;
+	}
+	 
+	
+}
